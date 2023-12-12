c0483b3a86ae064be94c1c263c1687fca88712c4
jhanaveenk <jhanaveen255@gmail.com>
2023-12-12 13:04:35 +0530

    PurchaseOrdersDetailView and OrderAcknowledgeView created

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('vendors', VendorsView.as_view(), name='vendor-view'),
     path('vendors/<int:vendor_id>', VendorsDeatilView.as_view(), name='vendor-detail-view'),
     path('purchase_orders', PurchaseOrdersView.as_view(), name='purchase-orders'),
+    path('purchase_orders/<int:po_id>', PurchaseOrdersDetailView.as_view(), name='purchase-detail-view'),
+    path('purchase_orders/<int:po_id>/acknowledge', OrderAcknowledgeView.as_view(), name='order-acknowledge')
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import VendorsSerializer, PurchaseOrdersSerializer
@@ -8,15 +9,15 @@ from .models import Vendors, PurchaseOrders
 class VendorsView(APIView):
 
     def get(self, request, *args, **kwargs):
-        queryset = Vendors.objects.all().order_by('id')
-        serializer = VendorsSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        vendor_queryset = Vendors.objects.all().order_by('id')
+        ven_serializer = VendorsSerializer(vendor_queryset, many=True)
+        return Response(ven_serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request, *args, **kwargs):
-        serializer = VendorsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        ven_serializer = VendorsSerializer(data=request.data)
+        if ven_serializer.is_valid():
+            ven_serializer.save()
+            return Response(ven_serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
 
@@ -25,29 +26,29 @@ class VendorsDeatilView(APIView):
     def get(self, request, *args, **kwargs):
         vendor_id = kwargs["vendor_id"]
         try:
-            queryset = Vendors.objects.get(id=vendor_id)
+            vendor_instance = Vendors.objects.get(id=vendor_id)
         except Vendors.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = VendorsSerializer(queryset)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        ven_serializer = VendorsSerializer(vendor_instance)
+        return Response(ven_serializer.data, status=status.HTTP_200_OK)
     
     def put(self, request, *args, **kwargs):
         vendor_id = kwargs["vendor_id"]
         try:
-            queryset = Vendors.objects.get(id=vendor_id)
+            vendor_instance = Vendors.objects.get(id=vendor_id)
         except Vendors.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = VendorsSerializer(queryset, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        ven_serializer = VendorsSerializer(vendor_instance, data=request.data, partial=True)
+        if ven_serializer.is_valid():
+            ven_serializer.save()
+            return Response(ven_serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, *args, **kwargs):
         vendor_id = kwargs["vendor_id"]
         try:
-            queryset = Vendors.objects.get(id=vendor_id)
-            queryset.delete()
+            vendor_instance = Vendors.objects.get(id=vendor_id)
+            vendor_instance.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Vendors.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -56,6 +57,78 @@ class VendorsDeatilView(APIView):
 class PurchaseOrdersView(APIView):
 
     def get(self, request, *args, **kwargs):
-        queryset = PurchaseOrders.objects.all()
-        serializer = PurchaseOrdersSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        purchase_queryset = PurchaseOrders.objects.all()
+        po_serializer = PurchaseOrdersSerializer(purchase_queryset, many=True)
+        return Response(po_serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        po_serializer = PurchaseOrdersSerializer(data=request.data)
+        if po_serializer.is_valid():
+            po_serializer.save()
+            return Response(po_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+"""
+Payload:
+{
+    "po_number": "ucebva6aceuaijk",
+    "vendor": 8,
+    "items": [
+        {"name": "Item1", "quantity": 10},
+        {"name": "Item2", "quantity": 5}
+    ],
+    "quantity": 15,
+    "status": "pending"
+}
+"""
+    
+class PurchaseOrdersDetailView(APIView):
+    
+    def get(self, request, *args, **kwargs):
+        po_id = kwargs["po_id"]
+        try:
+            purchase_instance = PurchaseOrders.objects.get(id=po_id)
+        except PurchaseOrders.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        po_serializer = PurchaseOrdersSerializer(purchase_instance)
+        return Response(po_serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, *args, **kwargs):
+        po_id = kwargs["po_id"]
+        try:
+            purchase_instance = PurchaseOrders.objects.get(id=po_id)
+        except PurchaseOrders.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        po_serializer = PurchaseOrdersSerializer(purchase_instance, data=request.data, partial=True)
+        if po_serializer.is_valid():
+            po_serializer.save()
+            return Response(po_serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    """
+    PAYLOAD
+    {
+    "delivery_date" : "2023-12-15T08:00:10.000000Z",
+    "status":"completed"
+    }
+    """
+
+    def delete(self, request, *args, **kwargs):
+        po_id = kwargs["po_id"]
+        try:
+            purchase_instance = PurchaseOrders.objects.get(id=po_id)
+            purchase_instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except PurchaseOrders.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+class OrderAcknowledgeView(APIView):
+    def get(self, request, *args, **kwargs):
+        po_id = kwargs["po_id"]
+        try:
+            purchase_instance = PurchaseOrders.objects.get(id=po_id)
+        except PurchaseOrders.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        purchase_instance.acknowledgment_date = timezone.now()
+        purchase_instance.save()
+        return Response("Order acknowledged sucessfully", status=status.HTTP_202_ACCEPTED)
