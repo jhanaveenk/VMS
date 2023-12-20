313f7904b0860ed31a52d4c99330edcbc8dac73e
jhanaveenk <jhanaveen255@gmail.com>
2023-12-20 18:59:04 +0530

    modification in vendor model

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class Vendors(models.Model):
     vendor_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     on_time_delivery_rate = models.FloatField(null=True, blank=True)
     quality_rating_avg = models.FloatField(null=True, blank=True)
-    average_response_time = models.FloatField(null=True, blank=True)
+    average_response_time = models.DurationField(null=True, blank=True)
     fulfillment_rate = models.FloatField(null=True, blank=True)
 
     def __str__(self):
--- base/signals.py
+++ base/signals.py
@@ -8,19 +8,15 @@ from .models import PurchaseOrders, HistoricalPerformaces, Vendors
 
 @receiver(post_save, sender=PurchaseOrders)
 def update_vendor_performance_on_save(sender, instance, created, **kwargs):
-    print(instance)
     vendor = instance.vendor
-
     if instance.status == 'completed':
         update_on_time_delivery_rate(vendor)
         instance.delivery_date = timezone.now()
-
+        instance.save()
     if instance.quality_rating is not None:
         update_quality_rating_avg(vendor)
-
     if instance.acknowledgment_date:
         update_average_response_time(vendor)
-
     update_fulfilment_rate(vendor)
 
 
@@ -40,12 +36,14 @@ def update_on_time_delivery_rate(vendor):
     on_time_deliveries = completed_pos.filter(delivery_date__lte=timezone.now())
     on_time_delivery_rate = round(on_time_deliveries.count() / completed_pos.count(), 2) if completed_pos.count() > 0 else 0.0
     HistoricalPerformaces.objects.update_or_create(vendor=vendor, defaults={'on_time_delivery_rate': on_time_delivery_rate})
+    Vendors.objects.filter(pk=vendor.pk).update(on_time_delivery_rate=on_time_delivery_rate)
 
 
 def update_quality_rating_avg(vendor):
     completed_pos = PurchaseOrders.objects.filter(vendor=vendor, status='completed', quality_rating__isnull=False)
     quality_rating_avg = completed_pos.aggregate(Avg('quality_rating'))['quality_rating__avg']
     HistoricalPerformaces.objects.update_or_create(vendor=vendor, defaults={'quality_rating_avg': round(quality_rating_avg, 2)})
+    Vendors.objects.filter(pk=vendor.pk).update(quality_rating_avg=round(quality_rating_avg, 2))
 
 
 def update_average_response_time(vendor):
@@ -54,6 +52,7 @@ def update_average_response_time(vendor):
         response_time=Coalesce(F('acknowledgment_date') - F('issue_date'), timezone.timedelta())
     ).aggregate(Avg('response_time'))['response_time__avg']
     HistoricalPerformaces.objects.update_or_create(vendor=vendor, defaults={'average_response_time': response_times})
+    Vendors.objects.filter(pk=vendor.pk).update(average_response_time=response_times)
 
 
 def update_fulfilment_rate(vendor):
@@ -61,3 +60,4 @@ def update_fulfilment_rate(vendor):
     successful_fulfillments = all_pos.filter(status='completed', issue_date__isnull=False, acknowledgment_date__isnull=False)
     fulfilment_rate = round(successful_fulfillments.count() / all_pos.count(), 2) if all_pos.count() > 0 else 0.0
     HistoricalPerformaces.objects.update_or_create(vendor=vendor,  defaults={'fulfillment_rate': fulfilment_rate})
+    Vendors.objects.filter(pk=vendor.pk).update(fulfillment_rate=fulfilment_rate)
